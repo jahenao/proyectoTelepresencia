9a8da0293801ed6a47c6e3fb2f8b42056bcd87fb
jahenao <julian123hh@hotmail.com>
2012-09-06 20:30:55 -0500

    se envia el proy

---
+++ proyectoPrueba/src/Principal.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author JULIAN HENAO
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hola Mundo");
+	}
+
+}
